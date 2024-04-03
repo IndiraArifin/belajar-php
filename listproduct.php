ac402f990e52efb52650f255963e5ced001c2860
IndiraArifin <161846577+IndiraArifin@users.noreply.github.com>
2024-04-04 00:30:34 +0700

    Add files via upload

---
+++ listproduct.php
@@ -0,0 +1,273 @@
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Product</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Product</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Default box -->
+      <div class="card card-solid">
+        <div class="card-body pb-0">
+          <div class="row">
+            <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch flex-column">
+              <div class="card bg-light d-flex flex-fill">
+                <div class="card-header text-muted border-bottom-0">
+                  Product
+                </div>
+                <div class="card-body pt-3">
+                  <div class="row">
+                    <div class="col-7">
+                      <h2 class="lead"><b>GRAY SHIRT</b></h2>
+                      <p class="text-muted text-sm"><b>About: </b> Pakaian Wanita </p>
+                      <ul class="ml-4 mb-0 fa-ul text-muted">
+                        <li class="small"><span class="fa-li"><i class="fas fa-lg fa-money-bill"></i></span> Rp. 200.000,00</li>
+                        <li class="small"><span class="fa-li"><i class="fas fa-lg fa-search"></i></span> Pakaian Wanita</li>
+                      </ul>
+                    </div>
+                    <div class="col-5 text-center">
+                      <img src="image/1.png" alt="user-avatar" class="img-circle img-fluid">
+                    </div>
+                  </div>
+                </div>
+                <div class="card-footer">
+                  <div class="text-right">
+                    <a href="#" class="btn btn-sm btn-primary">
+                      <i class="fas fa-user"></i> View Detail
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch flex-column">
+              <div class="card bg-light d-flex flex-fill">
+                <div class="card-header text-muted border-bottom-0">
+                  Product
+                </div>
+                <div class="card-body pt-3">
+                  <div class="row">
+                    <div class="col-7">
+                      <h2 class="lead"><b>PANT GIRL</b></h2>
+                      <p class="text-muted text-sm"><b>About: </b> Pakaian Wanita </p>
+                      <ul class="ml-4 mb-0 fa-ul text-muted">
+                        <li class="small"><span class="fa-li"><i class="fas fa-lg fa-money-bill"></i></span> Rp.250.000,00</li>
+                        <li class="small"><span class="fa-li"><i class="fas fa-lg fa-search"></i></span> Pakaian Wanita</li>
+                      </ul>
+                    </div>
+                    <div class="col-5 text-center">
+                      <img src="image/2.png" alt="user-avatar" class="img-circle img-fluid">
+                    </div>
+                  </div>
+                </div>
+                <div class="card-footer">
+                  <div class="text-right">
+                    <a href="#" class="btn btn-sm btn-primary">
+                      <i class="fas fa-user"></i> View Detail
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch flex-column">
+              <div class="card bg-light d-flex flex-fill">
+                <div class="card-header text-muted border-bottom-0">
+                  Product
+                </div>
+                <div class="card-body pt-3">
+                  <div class="row">
+                    <div class="col-7">
+                      <h2 class="lead"><b>POLO SHIRT GIRL</b></h2>
+                      <p class="text-muted text-sm"><b>About: </b> Pakaian Wanita </p>
+                      <ul class="ml-4 mb-0 fa-ul text-muted">
+                        <li class="small"><span class="fa-li"><i class="fas fa-lg fa-money-bill"></i></span> Rp.290.000,00</li>
+                        <li class="small"><span class="fa-li"><i class="fas fa-lg fa-search"></i></span> Pakaian Wanita</li>
+                      </ul>
+                    </div>
+                    <div class="col-5 text-center">
+                      <img src="image/3.png" alt="user-avatar" class="img-circle img-fluid">
+                    </div>
+                  </div>
+                </div>
+                <div class="card-footer">
+                  <div class="text-right">
+
+                    <a href="#" class="btn btn-sm btn-primary">
+                      <i class="fas fa-user"></i> View Detail
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch flex-column">
+              <div class="card bg-light d-flex flex-fill">
+                <div class="card-header text-muted border-bottom-0">
+                  Product
+                </div>
+                <div class="card-body pt-3">
+                  <div class="row">
+                    <div class="col-7">
+                      <h2 class="lead"><b>SHIRT GIRL</b></h2>
+                      <p class="text-muted text-sm"><b>About: </b> Pakaian Wanita </p>
+                      <ul class="ml-4 mb-0 fa-ul text-muted">
+                        <li class="small"><span class="fa-li"><i class="fas fa-lg fa-money-bill"></i></span> Rp.200.000,00</li>
+                        <li class="small"><span class="fa-li"><i class="fas fa-lg fa-search"></i></span> Pakaian Wanita</li>
+                      </ul>
+                    </div>
+                    <div class="col-5 text-center">
+                      <img src="image/4.png" alt="user-avatar" class="img-circle img-fluid">
+                    </div>
+                  </div>
+                </div>
+                <div class="card-footer">
+                  <div class="text-right">
+
+                    <a href="#" class="btn btn-sm btn-primary">
+                      <i class="fas fa-user"></i> View Detail
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch flex-column">
+              <div class="card bg-light d-flex flex-fill">
+                <div class="card-header text-muted border-bottom-0">
+                  Product
+                </div>
+                <div class="card-body pt-3">
+                  <div class="row">
+                    <div class="col-7">
+                      <h2 class="lead"><b>TUNIC SHIRT GIRL</b></h2>
+                      <p class="text-muted text-sm"><b>About: </b> Pakaian Wanita </p>
+                      <ul class="ml-4 mb-0 fa-ul text-muted">
+                        <li class="small"><span class="fa-li"><i class="fas fa-lg fa-money-bill"></i></span> Rp.200.000,00</li>
+                        <li class="small"><span class="fa-li"><i class="fas fa-lg fa-search"></i></span> Pakaian Wanita</li>
+                      </ul>
+                    </div>
+                    <div class="col-5 text-center">
+                      <img src="image/5.png" alt="user-avatar" class="img-circle img-fluid">
+                    </div>
+                  </div>
+                </div>
+                <div class="card-footer">
+                  <div class="text-right">
+
+                    <a href="#" class="btn btn-sm btn-primary">
+                      <i class="fas fa-user"></i> View Detail
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch flex-column">
+              <div class="card bg-light d-flex flex-fill">
+                <div class="card-header text-muted border-bottom-0">
+                  Product
+                </div>
+                <div class="card-body pt-3">
+                  <div class="row">
+                    <div class="col-7">
+                      <h2 class="lead"><b>SHIRT MEN</b></h2>
+                      <p class="text-muted text-sm"><b>About: </b> Pakaian Pria</p>
+                      <ul class="ml-4 mb-0 fa-ul text-muted">
+                        <li class="small"><span class="fa-li"><i class="fas fa-lg fa-money-bill"></i></span> Rp.300.000,00</li>
+                        <li class="small"><span class="fa-li"><i class="fas fa-lg fa-search"></i></span> Pakaian Pria</li>
+                      </ul>
+                    </div>
+                    <div class="col-5 text-center">
+                      <img src="image/6.png" alt="user-avatar" class="img-circle img-fluid">
+                    </div>
+                  </div>
+                </div>
+                <div class="card-footer">
+                  <div class="text-right">
+
+                    <a href="#" class="btn btn-sm btn-primary">
+                      <i class="fas fa-user"></i> View Detail
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch flex-column">
+              <div class="card bg-light d-flex flex-fill">
+                <div class="card-header text-muted border-bottom-0">
+                  Product
+                </div>
+                <div class="card-body pt-3">
+                  <div class="row">
+                    <div class="col-7">
+                      <h2 class="lead"><b>UNDERSHIRT</b></h2>
+                      <p class="text-muted text-sm"><b>About: </b> Pakaian Wanita </p>
+                      <ul class="ml-4 mb-0 fa-ul text-muted">
+                        <li class="small"><span class="fa-li"><i class="fas fa-lg fa-money-bill"></i></span> Rp.200.000,00</li>
+                        <li class="small"><span class="fa-li"><i class="fas fa-lg fa-search"></i></span> Pakaian Wanita</li>
+                      </ul>
+                    </div>
+                    <div class="col-5 text-center">
+                      <img src="image/7.png" alt="user-avatar" class="img-circle img-fluid">
+                    </div>
+                  </div>
+                </div>
+                <div class="card-footer">
+                  <div class="text-right">
+
+                    <a href="#" class="btn btn-sm btn-primary">
+                      <i class="fas fa-user"></i> View Detail
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch flex-column">
+              <div class="card bg-light d-flex flex-fill">
+                <div class="card-header text-muted border-bottom-0">
+                  Product
+                </div>
+                <div class="card-body pt-3">
+                  <div class="row">
+                    <div class="col-7">
+                      <h2 class="lead"><b>WESTERN SHIRT</b></h2>
+                      <p class="text-muted text-sm"><b>About: </b> Pakaian Wanita </p>
+                      <ul class="ml-4 mb-0 fa-ul text-muted">
+                        <li class="small"><span class="fa-li"><i class="fas fa-lg fa-money-bill"></i></span> Rp.200.000,00</li>
+                        <li class="small"><span class="fa-li"><i class="fas fa-lg fa-search"></i></span> Pakaian Wanita</li>
+                      </ul>
+                    </div>
+                    <div class="col-5 text-center">
+                      <img src="image/8.png" alt="user-avatar" class="img-circle img-fluid">
+                    </div>
+                  </div>
+                </div>
+                <div class="card-footer">
+                  <div class="text-right">
+
+                    <a href="#" class="btn btn-sm btn-primary">
+                      <i class="fas fa-user"></i> View Detail
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /.card-body -->
+
+      </div>
+      <!-- /.card -->
+
+    </section>
+    <!-- /.content -->
+  </div>
---
+++ template.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include('header.php');?>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+  <!-- Navbar -->
+  <?php include('navbar.php')?>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+
+    <!-- Sidebar -->
+    <?php include('sidebar.php')?>
+    <!-- /.sidebar -->
+  </aside>
+
+    <?php include('listproduct.php');?>
+
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+  <?php include('footer.php');?>
+</body>
+</html>
